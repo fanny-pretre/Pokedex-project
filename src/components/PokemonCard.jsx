bcd99319b1b9e2dafdfbffa341af67f08fb5a6ba
FANNY PRETRE <fanny.pretre@gmail.com>
2024-03-26 16:04:14 +0100

    Modified PokemonCard Component to display correct Pokemon based on pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,20 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-        <figcaption> Bulbasaur </figcaption>
+    const pokemon = pokemonList[1]; 
+    return <figure className="card">
+        {pokemon.imgSrc === undefined ? <p> "???" </p> : <img src= {pokemon.imgSrc} className="card-img" alt={pokemon.name}/>}
+        <figcaption> {pokemon.name} </figcaption>
     </figure>;
 }
 
